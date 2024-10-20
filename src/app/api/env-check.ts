2871a83e29ee8cd14ac29a8629cd62a0b661e554
Younes43 <elbouzekraoui.younes@gmail.com>
2024-10-20 18:10:22 -0400

    modified docker to adapt to could run + changes in apis logic

--- src/app/api/env-check.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/env-check.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({
-        NEXT_PUBLIC_GOOGLE_PLACES_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY
-    });
-}
